ed1ee05bb67ec18c045f70125aa4f84bc48d4e9f
yuchuan zhuang <yuchuan.zhuang@mindgeek.com>
2019-11-25 13:20:51 -0500

    class-based accordion comp sample

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Accordion from './Accordion';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div>
+      <h1>Accordion Demo</h1>
+      <Accordion allowMultipleOpen>
+        <div label='Alligator Mississippiensis' isOpen>
+          <p>
+            <strong>Common Name:</strong> American Alligator
+          </p>
+          <p>
+            <strong>Distribution:</strong> Texas to North Carolina, US
+          </p>
+          <p>
+            <strong>Endangered Status:</strong> Currently Not Endangered
+          </p>
+        </div>
+        <div label='Alligator Sinensis'>
+          <p>
+            <strong>Common Name:</strong> Chinese Alligator
+          </p>
+          <p>
+            <strong>Distribution:</strong> Eastern China
+          </p>
+          <p>
+            <strong>Endangered Status:</strong> Critically Endangered
+          </p>
+        </div>
+      </Accordion>
     </div>
   );
 }
